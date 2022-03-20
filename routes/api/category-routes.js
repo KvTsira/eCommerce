b464e1d2620a699e53b2a2219b28483b6a349023
KvTsira <kvtsira@gmail.com>
2022-03-20 17:07:01 -0400

    Final commitment

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models/index');
+const { Category, Product } = require('../../models/');
 
 // The `/api/categories` endpoint
 
